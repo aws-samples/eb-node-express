c0b7b92f403c4b5d576ecf1e7478804b405b0f7e
Evan Brown <evandbrown@gmail.com>
2013-07-11 15:42:18 -0700

    Add database support

--- routes/hike.js
+++ routes/hike.js
@@ -1,6 +1,14 @@
+var uuid = require('node-uuid');
+
 exports.index = function(req, res) {
- res.render('hike', {title: 'My Hiking Log'});
+  res.app.get('connection').query( 'SELECT * FROM HIKES', function(err, rows) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(JSON.stringify(rows));
+      res.render('hike', {title: 'My Hiking Log', hikes: rows});
+  }});
 };
 
-exports.add_hike = function(req, res) {
+exports.add_hike = function(req, res){
 };
