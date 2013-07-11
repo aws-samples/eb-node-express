6dc52fdfdbee03e3b976db7b8d8601b505ad0a0f
Evan Brown <evandbrown@gmail.com>
2013-07-11 16:26:23 -0700

    Ability to add new hike entry

--- routes/hike.js
+++ routes/hike.js
@@ -11,4 +11,16 @@ exports.index = function(req, res) {
 };
 
 exports.add_hike = function(req, res){
+  var input = req.body.hike;
+  var hike = { HIKE_DATE: new Date(), ID: uuid.v4(), NAME: input.NAME,
+      LOCATION: input.LOCATION, DISTANCE: input.DISTANCE, WEATHER: input.WEATHER};
+
+  console.log('Request to log hike:' + JSON.stringify(hike));
+  req.app.get('connection').query('INSERT INTO HIKES set ?', hike, function(err) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.redirect('/hikes');
+    }
+  });
 };
